15fb423d12550ac302faa198cd66fc3ea153dd7c
Saikat Biswas <saikat1236@gmail.com>
2024-05-04 13:36:28 +0530

    fixed

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,17 +1,23 @@
 import React, { Fragment } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
 
 import Hero from "../components/Hero";
 import Leads from "../components/Content";
 
-const Home = () => (
-  <Fragment>
-    <Hero />
-    <hr />
-    {!isAuthenticated && (
-      <Leads />
-      )}
-    
-  </Fragment>
-);
+const Home = () => {
+  const { isAuthenticated } = useAuth0();  // Use the Auth0 hook to get the authentication status
+
+  return (
+    <Fragment>
+      <Hero />
+      <hr />
+      {isAuthenticated && <Leads />}
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <h4>  Please login to check all leads</h4>
+      
+      </div>
+    </Fragment>
+  );
+};
 
 export default Home;
